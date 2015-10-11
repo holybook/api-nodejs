daea511c3d639ebf8db1dc3f894c05f958f6d423
Hannes Widmoser <widmoser@gmail.com>
2015-10-11 22:22:17 +0200

    added endpoint: GET /author

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 var express = require('express');
+var elasticsearch = require('elasticsearch');
+
 var app = express();
+var client = new elasticsearch.Client({
+    host: 'localhost:9200',
+    log: 'trace'
+});
+
+app.get('/author', function(req, res) {
+    client.search({
+        index: 'en',
+        type: 'author',
+        body: {
+            "query": {
+                "match_all": {}
+            }
+        }
+    }).then(function (o) {
+        res.send(o.hits.hits.map(a => a._source));
+    });
+});
 
 var server = app.listen(3000, function() {
     var host = server.address().address;
